61d59eac5acf255bf79f73fbf0e3f55532269525
Kyryl Frosyniak <kfrosyniak@interia.pl>
2025-09-25 14:40:20 +0200

    removing dist

--- apps/api/dist/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4000;
-(0, server_1.buildServer)().then((app) => {
-    app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
-        console.log(`Server listening on port ${PORT}`);
-    });
-});
